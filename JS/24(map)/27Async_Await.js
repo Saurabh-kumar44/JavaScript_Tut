939470542d54b4dc68d7e6272ebbd9337de109ed
saurav <sauravvermas1097v@gmail.com>
2023-04-01 01:04:30 +0530

    Parallel execution

--- JS/24(map)/27Async_Await.js
+++ JS/24(map)/27Async_Await.js
@@ -6,3 +6,39 @@
 // })
 
 
+async function kisna(){
+    let delhiWeather = new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve("27 degrees");
+        },2000)
+    })
+    let bangloreWeather = new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve("21 degrees");
+        },5000)
+    })
+    // delhiWeather.then(alert); //the resolve value is get into the alert
+    // bangloreWeather.then(alert);//the resolve value is get into the alert
+
+    console.log("Fetching delhi's weather please wait...");
+    let delhiW = await delhiWeather;// here resolve promise came here and waits until the next promise came and waits(or we can say the fullfilled promise came here and wait) we can make the promised waits for a time
+    console.log("Fetched delhi's weather: "+delhiW);
+
+    console.log("Fetching banglore's weather please wait...");
+    let bangloreW = await bangloreWeather;
+    console.log("Fetched banglore's: " +bangloreW);
+    return [delhiW, bangloreW];
+
+}
+
+const krishna =()=>{
+    console.log("hey i am krishna and im not waiting"); 
+}
+console.log("Welcome to weather control room!");
+kisna();
+let a = kisna();
+let b = krishna();//all the process runs parallely
+// console.log(a);
+a.then((value)=>{
+    console.log(value);
+})
