8ae349e5f61fb0b84f240be2dceca8073ffa6523
saurav <sauravvermas1097v@gmail.com>
2023-04-18 16:38:26 +0530

    array into object

--- JS/24(map)/48Destructuring.js
+++ JS/24(map)/48Destructuring.js
@@ -14,5 +14,10 @@
 // let [a, , ,...rest] = arr;
 // console.log(a, rest);
 
-let {a, b} = {a: 1, b:5}
-console.log(a,b);
+// let {a, b} = {a: 1, b:5}
+// console.log(a,b);
+
+//Spead syntax- allows an iterable such as an array or string to be expanded in places where zero or more arguments are expexted, In an object literal, the soread syntax enumerated the proprties of an object and adds the key-value pairs to the ibject being created
+let arr1 = [3,5,6]
+let obj1 = { ...arr1 } //making into object
+console.log(obj1);
