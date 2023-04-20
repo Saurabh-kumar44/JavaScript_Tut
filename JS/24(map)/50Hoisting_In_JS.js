40a9fad1eb6c62e606c98a5b17d95d5b71e42a22
saurav <sauravvermas1097v@gmail.com>
2023-04-20 20:25:47 +0530

    Hoisting in JS

---
+++ JS/24(map)/50Hoisting_In_JS.js
@@ -0,0 +1,28 @@
+
+// greet();
+// function greet(){
+//     console.log("hoisting");
+// }
+
+// // greet();//here this seems to be get at the top and still works in JS
+
+
+console.log(a); //still not shows error(due to hoisting)
+greet();
+// function greet(){
+//     console.log("hoisting");
+// }
+// const greet = () =>{ //this is a function expression (so cannot hoisted)
+//     console.log("hoisting");
+// }
+const greet = function(){ //this is a function expression (so cannot hoisted)
+    console.log("hoisting");
+}
+
+// greet();
+var a = 9;
+// let a = 9; // we cannot use a before initilization
+// const a = 9; // we cannot use a before initilization
+console.log(a);
+
+
