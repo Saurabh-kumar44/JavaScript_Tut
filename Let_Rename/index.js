5c28166aa8168e7cc6b08493d9324694690740ba
saurav <sauravvermas1097v@gmail.com>
2023-04-28 17:08:42 +0530

    bulk renamer

--- Let_Rename/index.js
+++ Let_Rename/index.js
@@ -1,17 +1,29 @@
 const fs = require('fs');
+const path = require('path');
 
 const replaceThis = "kisna"
 const replaceWith = "Saurav"
+const preview = false
+const folder = __dirname; //it gives the current path of the directory
+
 
 try {
-  fs.readdir("data", (err,data)=>{
+  fs.readdir(folder, (err,data)=>{
     console.log(data);
     for(let index=0; index<data.length; index++){
       const item  = data[index];
-      let newFile = "data/" + item.replaceAll(replaceThis, replaceWith);
-      fs.rename("data/"+ item, newFile, ()=>{
-        console.log("rename succuss");
+      let oldFile = path.join(folder, item);
+      let newFile = path.join(folder, item.replaceAll(replaceThis, replaceWith));
+      if(!preview){
+        fs.rename(oldFile, newFile, ()=>{
+        console.log("rename success");
       })
+      }
+      else{
+        if("data "/ +item !== newFile){
+          console.log("data/ " + item + " will be renamed to this " +newFile);
+        }
+      }
     }
   });
 } catch (err) {
